acca590c1de87cf206631ec17b56f184b927f5ff
Darren.fan <2511132466@qq.com>
2019-03-11 19:57:56 +0800

    add blog

---
+++ src/main.js
@@ -0,0 +1,50 @@
+import Vue from 'vue'
+import App from './App'
+import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
+import Routes from './router'
+
+Vue.config.productionTip = false
+
+Vue.use(VueResource)
+Vue.use(VueRouter)
+// Vue.directive('rainbow',{
+//   bind(el,binding,vnode){
+//     el.style.color = '#'+Math.random().toString(16).slice(2,8);
+//   }
+// })
+
+Vue.directive('theme',{
+  bind(el,binding,vnod){
+    if (binding.value == 'wide') {
+      el.style.maxWidth = '1260px';
+    }else if (binding.value == 'narrow'){
+      el.style.maxWidth = '560px';
+    }
+
+    if (binding.arg == 'column'){
+      el.style.background = '#6677cc'
+      el.style.padding = '20px'
+    }
+  }
+})
+
+// Vue.filter('to-uppercase',function (value) {
+//   return value.toUpperCase()
+// })
+
+// Vue.filter('snippet',function (value) {
+//   return value.slice(0,100)+'...'
+// })
+/* eslint-disable */
+
+const router = new VueRouter({
+  routes:Routes,
+  mode:"history"
+})
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>',
+  router:router
+})
